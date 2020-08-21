2c6e429238cd76e413eee22b6dbcd0c506985870
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-08-20 21:32:30 -0400

    Animatable

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -6,6 +6,7 @@ import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 import Swipeout from 'react-native-swipeout';
 import { deleteFavorite } from "../redux/ActionCreators";
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -46,20 +47,22 @@ class Favorites extends Component {
                                     onPress: () => this.props.deleteFavorite(item.id)
                                 }
                             ],
-                            {cancelable: false }
+                            { cancelable: false }
                         )
-                    }                    
+                    }
                 }
             ];
-            
+
             return (
                 <Swipeout right={rightButton} autoClose={true}>
-                    <ListItem
-                        title={item.name}
-                        subtitle={item.description}
-                        leftAvatar={{ source: { uri: baseUrl + item.image } }}
-                        onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-                    />
+                    <Animatable.View animation='fadeInRightBig' duration={2000}>
+                        <ListItem
+                            title={item.name}
+                            subtitle={item.description}
+                            leftAvatar={{ source: { uri: baseUrl + item.image } }}
+                            onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                        />
+                    </Animatable.View>
                 </Swipeout>
             );
         };
